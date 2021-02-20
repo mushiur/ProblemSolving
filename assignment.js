3a3d93b5c638cce2e31bbe4e4746ef11cefc8284
newlyyyy <rifat1520154@gmail.com>
2021-02-21 00:14:01 +0600

    done

--- assignment.js
+++ assignment.js
@@ -38,4 +38,61 @@ function budgetCalculator(clock, phone, laptop) {
 budgetCalculator(1, 2, 3);
 // console.log(result);
 
+// hotelCost
+
+function hotelCost(days) {
+    var firstTenDays;
+    var secondTenDays;
+    var uptoTwentyDays;
+    var totalCost;
+
+    if (days >= 0 ) {
+        if (days <= 10) {
+            firstTenDays = days * 100;
+            totalCost = firstTenDays;
+
+            return totalCost;
+
+        } else if (days <= 20) {
+            firstTenDays = 10 * 100;
+            secondTenDays = (days - 10) * 80;
+            totalCost = firstTenDays + secondTenDays;
+
+            return totalCost;
+
+        } else if (days > 20) {
+            firstTenDays = 10 * 100;
+            secondTenDays = 10 * 80;
+            uptoTwentyDays = (days - 20) * 50;
+            totalCost = firstTenDays + secondTenDays + uptoTwentyDays;
+            // total costing ;
+
+            return totalCost;
+        }
+
+    } else {
+        return "days can't be negative value";
+    }
+}
+
+hotelCost(12);
+
+// megaFriend
+
+
+function megaFriend(friends) {
+    let word = "";
+    for (let i = 0; i < friends.length; i++) {
+      if (word.length < friends[i].length) {
+        word = friends[i];
+      }
+      else{
+          return "undefined"; 
+      }
+    }
+    return word;
+  }
+//   var result1 = 
+  megaFriend(['rifatRahman', 'istak', 'niloy']);
+//   console.log(result1);
 
